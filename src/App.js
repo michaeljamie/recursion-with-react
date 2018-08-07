9d404dea2e70e4aa7b9daf40c3b5af0cb83c3af9
Missy Beutler <missyjbeutler@gmail.com>
2018-08-07 09:42:27 -0600

    starting boiler

--- src/App.js
+++ src/App.js
@@ -6,29 +6,13 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
-    this.commentBuild = this.commentBuild.bind(this)
-  }
 
-  commentBuild(data) {
-    return data.map(e => {
-      if(e.subComments) {
-        return (
-          <Comment data={ e } key={ e.id }>
-            { this.commentBuild(e.subComments) }
-          </Comment>
-        )
-      } else {
-        return <Comment data={ e } key={ e.id }/>
-      }
-    })
   }
 
   render() {
-    const comments = this.commentBuild(data);
     return (
       <div>
-        <h1>Comment Board</h1>
-        { comments }
+
       </div>
     );
   }
